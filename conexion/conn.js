8ceb28863efddf158c72afe1cfead90d75b7e434
Santiago Arjona <wilberth2010@hotmail.com>
2020-07-17 20:24:01 -0500

    -Implementacion de metodo PUT. -Mejora de codigo. -Implementacion de base de datos en servidor en conn.js.

--- conexion/conn.js
+++ conexion/conn.js
@@ -1,15 +1,25 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database:'api_rest'
+  host     : 'us-cdbr-east-02.cleardb.com',
+  user     : 'be6abcdb612529',
+  password : 'f8880b7e',
+  database:'heroku_e12b52604cab367'
 });
+
+//User: be6abcdb612529
+//Password: f8880b7e
+//Host: us-cdbr-east-02.cleardb.com
 
 connection.connect(
     (err)=>{
      if(!err){ console.log("conexion establecida")} else { console.log("conexion fallida")}  
     }
 );
+
+//La conexion al servidor se cierra cada 10s despues de inactividad, se implementó un interval para que
+//la conexion siga activa.
+setInterval(function () {
+  connection.query('SELECT 1');
+}, 5000);
 
 module.exports = connection;
--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,64 @@ var conn = require("../conexion/conn");
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
+ try {
   conn.query("SELECT * FROM productos", (err, result) =>{
     res.json(result);
   });
+ } catch (error) {
+    res.status(500).send(error);
+ }
+});
+
+router.get('/GetProduct/:id', function(req, res, next) {
+  try {
+    const {id} = req.params;
+    conn.query("SELECT * FROM productos where ID = '"+id+"';", (err, result) =>{
+      res.json(result);
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.post('/',(req, res) => {
+try {
   const {title, imagen, descripcion, precio} = req.body;
   if(title && imagen && descripcion && precio){
     conn.query("INSERT INTO productos (ID, titulo, imagen, descripcion, precio) VALUES (NULL, '"+title+"', '"+imagen+"', '"+descripcion+"', '"+precio+"');", (err, result)=>{
       res.status(200).send("Producto agregado");
     });
-  }else{res.status(500).send()}
+  }else{res.status(500).send("Producto rechazado")}
+} catch (error) {
+  res.status(500).send(error)
+}
 });
 
 router.delete('/:id',(req, res) => {
-  const { id } = req.params;
-  if(id != null){
-    conn.query("DELETE FROM productos WHERE '"+id+"'", (err, result)=>{
-      res.status(200).send("Producto eliminado");
-    });
-  }else{res.status(500).send()}
+  try {
+    const { id } = req.params;
+    if(id != null){
+      conn.query("DELETE FROM productos WHERE '"+id+"'", (err, result)=>{
+        res.status(200).send("Producto eliminado");
+      });
+    }else{res.status(500).send()}
+  } catch (error) {
+    res.status(500).send(error)
+  }
+});
+
+router.put('/:id',(req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, imagen, descripcion, precio } = req.body;
+    if(title && imagen && descripcion && precio && id){
+      conn.query("UPDATE productos SET titulo = '"+title+"', imagen = '"+imagen+"', descripcion = '"+descripcion+"', precio = '"+precio+"' WHERE ID = '"+id+"';", (err, result)=>{
+        res.status(200).send(result);
+      });
+    }else{res.status(500).send()}
+  } catch (error) {
+    res.status(500).send(error)
+  }
 });
 
 module.exports = router;
